7c1835a33ca7e3ae9375c2685ddb783415e8cc9f
AriGochi <125834764+AriGochi@users.noreply.github.com>
2023-05-04 12:18:25 -0600

    Add files via upload

---
+++ LayoutsJava2208/src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Layout: Mira hacia arriba !");
+        //Ventana ventana = new Ventana("Layouts");
+
+        //VentanaGrid ventanaGrid = new VentanaGrid("Ventana layout cuadrada ");
+
+        VentanaSwing ventanaSwing = new VentanaSwing("Centigrados a Farenheid");
+    }
+}
