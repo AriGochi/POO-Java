b776ce6b655aca987894eaf05d352e659a4c13bb
AriGochi <125834764+AriGochi@users.noreply.github.com>
2023-05-04 12:23:24 -0600

    Add files via upload

---
+++ AutomovilDrive/src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+
+        Automovil auto1 = new Automovil();
+        Automovil auto2 = new Automovil();
+        System.out.println("El auto es:");
+    int edad = 12;
+    }
+}
